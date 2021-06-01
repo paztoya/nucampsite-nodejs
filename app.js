1137476877113b0c74a06d6064ae4e87a44e85fe
paztoya <daniel@paztoya.com>
2021-06-01 02:04:14 -0400

    Mongoose Population

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const partnerRouter = require("./routes/partnerRouter");
 
 const mongoose = require("mongoose");
 
-const url = "config.mongoUrl";
+const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
   useCreateIndex: true,
   useFindAndModify: false,
